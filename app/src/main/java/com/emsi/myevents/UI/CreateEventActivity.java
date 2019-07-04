dac13b642d7d7ec9d0911db79477ca6e8f5005c7
Zakabenha <zakabenha@gmail.com>
2019-07-04 16:55:33 +0100

    update
    Signed-off-by: Zakabenha <zakabenha@gmail.com>

---
+++ app/src/main/java/com/emsi/myevents/UI/CreateEventActivity.java
@@ -0,0 +1,14 @@
+package com.emsi.myevents.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event);
+    }
+}
